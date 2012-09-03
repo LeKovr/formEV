52d501b636af6e833d1dd365472252692d3efe03
Alexey Kovrizhkin <jean@tender.pro>
2012-09-04 02:52:56 +0400

    second editing, prerelease

--- README.md
+++ README.md
@@ -1,4 +1,55 @@
 formEV
 ======
 
-jQuery Form Edit/View plugin
+## Description
+
+formEV is a jQuery plugin which allows to show from in two modes, one for editing (as usual) and other for viewing.
+In View mode fields are showed as plain text values.
+When switching to Edit mode these values will be replaced with form fields and "Edit" button will be replaced by "Submit" and "Reset".
+User defined callback (as value of cbSave option) will be called when "Submit" clicked.
+
+## Form structure (via classes)
+
+    └── form container
+        └── form
+            ├── {field container} (class fldContainerClass), for each field
+            │   ├── field edit element (class fldEditClass)
+            │   └── field view container (class fldViewClass)
+            ├── Edit button (class btnEditOnClass)
+            ├── Submint button (classes btnEditOffClass, btnSaveClass)
+            └── Reset button  (classes btnEditOffClass, btnResetClass)
+
+Fields with type checkbox has slightly more complex field view container:
+
+    └── field view container  (class fldViewClass)
+        ├── container for checked=true (class chkOnClass)
+        └── container for checked=false (class chkOffClass)
+
+## Callbacks
+
+### onSubmit(container, cbSaveSuccess, cbFormDisable, cbFormEnable)
+
+Required. Called when user clicks Submit.
+
+Arguments:
+
+* container - form container object
+* cbSaveSuccess - function to call after successfull submitting, switches form to View mode
+* cbFormDisable - function to call after field serialize, disables all form fields
+* cbFormEnable - function to call after successfull submitting, enables disabled form fields back
+
+### onViewReady(container)
+
+Optional. Called when fields are ready to be shown in View mode.
+
+Arguments:
+
+* container - form container object
+
+## Requirements
+
+* jQuery (tested on v1.7.2)
+
+Example page requires also:
+
+* jQuery JSON plugin (tested on v2.3)
--- jquery.formev.js
+++ jquery.formev.js
@@ -1,106 +1,131 @@
 // ----------------------------------------------------------------------------
 /*
  * jQuery FormEV Plugin
- * version: 0.1 (2012-08-31)
+ * version: 0.2 (2012-09-04)
  *
  * This document is licensed as free software under the terms of the
  * MIT License: http://www.opensource.org/licenses/mit-license.php
  *
  * @copy 2012, Tender.Pro team
-
-    https://github.com/LeKovr/formEV
+ *
+ * Project repository: https://github.com/LeKovr/formEV
  */
 
 $.fn.formEV = function(options) {
-  var mode_edit = function (idx, container) {
-    container.find('.editable').each(function() {
-      $(this).children(options.names.childs[idx]).addClass('hide');
-      $(this).children(options.names.childs[1-idx]).removeClass('hide');
-    });
-    container.find(options.names.buttons[idx]).removeClass('hide');
-    container.find(options.names.buttons[1-idx]).addClass('hide');
-  }
 
-  function send(container) {
-    // пример отправи формы в alert
-    var form1 = container.children('form');
-    var params = $(form1).serializeObject();
-    var formid = form1.attr('id');
-    params['API_METHOD'] = form1.attr('id');
+  // ----------------------------------------------------------------------------
+  // Saving callback example
+  var onSubmitExample = function(container, cbSaveSuccess, cbFormDisable, cbFormEnable) {
+    var form = container.children('form');
+    var params = $(form).serializeArray();
     var post = $.toJSON(params);
-
-    container.find('.val').not(':disabled').addClass('disabled_till_send').attr("disabled","disabled");
-    // TODO: disable radio inside label
+    cbFormDisable(container);
     setTimeout(function() {
-      alert(post);
-      cb_ok(container);
+      alert("Ready to send:\n" + post);
+      cbFormEnable(container);
+      cbSaveSuccess(container);
     }, 2000);
-    // TODO: send data
-  }
+  };
 
-  var form2view = function(container) {
-    container.find('.editable').each(function() {
-      var field = $(this).children(options.names.childs[0]);
+  // ----------------------------------------------------------------------------
+  // All options listed here
+  options = $.extend(true, {
+    fldContainerClass:'editable'
+  , fldViewClass:     'view'
+  , fldEditClass:     'edit'
+  , btnEditOnClass:   'edit_on'
+  , btnEditOffClass:  'edit_off'
+  , btnSaveClass:     'save'
+  , btnResetClass:    'reset'
+  , chkOnClass:       'on'
+  , chkOffClass:      'off'
+  , onSubmit:         onSubmitExample   // Called when user clicks Submit
+  , onViewReady:      null              // Called when fields are ready to be shown in View mode
+  }, options);
+
+  // ----------------------------------------------------------------------------
+  // Set Edit/View mode
+  var setMode = function (isEdit, container) {
+    with (options) {
+      container.find('.' + fldContainerClass).each(function() {
+        $(this).children('.' + (isEdit ? fldViewClass : fldEditClass)).addClass('hide');
+        $(this).children('.' + (isEdit ? fldEditClass : fldViewClass)).removeClass('hide');
+      });
+      container.find('.' + (isEdit ? btnEditOnClass : btnEditOffClass)).addClass('hide');
+      container.find('.' + (isEdit ? btnEditOffClass : btnEditOnClass)).removeClass('hide');
+    };
+  };
+
+  // ----------------------------------------------------------------------------
+  // Fill Read mode data from inputs
+  var viewModeRefresh = function(container) {
+    container.find('.'+options.fldContainerClass).each(function() {
+      var field = $(this).children('.' + options.fldEditClass);
+      var needWrite = true;
+      var target = $(this).children('.' + options.fldViewClass);
       if (field.is('select')) {
         val = field.children('option:selected').text();
       } else if ( field.is('label') ) {  // radio
         val = field.find('input[type="radio"]:checked').parent().text();
       } else if ( field.is('input[type="checkbox"]') ) {
-        val = field.is(':checked') ? options.names.chkeckboxes[1] : options.names.chkeckboxes[0];
+        with (options) {
+          target.children('.' + (field.is(':checked') ? chkOffClass : chkOnClass)).addClass('hide');
+          target.children('.' + (field.is(':checked') ? chkOnClass : chkOffClass)).removeClass('hide');
+        }
+        needWrite = false;
       } else  {
         // text field
         val = field.val();
       }
-      $(this).children(options.names.childs[1]).text(val);
-
+      if (needWrite) {
+        target.text(val);
+      }
     });
-  }
+    if (options.onViewReady) { options.onViewReady(container) }
 
-  var cb_ok = function(container) {
-    form2view(container);
-    mode_edit(0, container);
-    container.find('.disabled_till_send').removeAttr("disabled").removeClass('disabled_till_send');
-  }
+  };
 
-  /* body */
+  // ----------------------------------------------------------------------------
+  // Switch to view mode after successfull submit
+  var cbSubmitSuccess = function(container) {
+    viewModeRefresh(container);
+    setMode(false, container);
+  };
+
+  // ----------------------------------------------------------------------------
+  // Disable fields for edit while saving
+  var cbFormDisable = function(container) {
+    container.find('.' + options.fldEditClass).not(':disabled').addClass('disabled_till_send').attr('disabled', 'disabled');
+    // disable radio inside label
+    container.find('.' + options.fldEditClass + ' :input').not(':disabled').addClass('disabled_till_send').attr('disabled', 'disabled');
+  };
+
+  // ----------------------------------------------------------------------------
+  // Enable back fields for edit after successfull saving
+  var cbFormEnable = function(container) {
+    container.find('.disabled_till_send').removeAttr('disabled').removeClass('disabled_till_send');
+  };
+
+  // ----------------------------------------------------------------------------
+  // Save form data
+  var submitForm = function(container) {
+    options.onSubmit(container, cbSubmitSuccess, cbFormDisable, cbFormEnable);
+  };
+
+  // ----------------------------------------------------------------------------
+  // Initialize containers at start
   this.each(function(){
     var row = $(this);
-    var form1 = row.children('form');
-    $("input[name='edit_btnn']").each(function(){ // перебираю все, т.к. не знаю как по form1 выйти на ее input
-      if ($(this.form).is(form1)) {
-        $(this).click(function(){ mode_edit(1, row); });
-      }
+    row.find('.' + options.btnEditOnClass).each(function(){
+      $(this).click(function(){ setMode(true, row); });
     });
-    $("input[name='e1']").each(function(){
-      if ($(this.form).is(form1)) {
-        $(this).click(function(){ mode_edit(0, row); return true; });
-      }
+    row.find('.' + options.btnResetClass).each(function(){
+      $(this).click(function(){ setMode(false, row); return true; });
     });
-    $("input[name='e2']").each(function(){
-      if ($(this.form).is(form1)) {
-        $(this).click(function(){ send(row); });
-      }
+      row.find('.' + options.btnSaveClass).each(function(){
+      $(this).click(function(){ submitForm(row); });
     });
-    form2view(row);
+    viewModeRefresh(row);
   });
 
-};
-// ----------------------------------------------------------------------------
-// PGWS API code
-$.fn.serializeObject = function()
-{
-    var o = {};
-    var a = this.serializeArray();
-    var formid = this.attr('id');
-    $.each(a, function() {
-      if (o[this.name]) {
-        if (!o[this.name].push) {
-          o[this.name] = [o[this.name]];
-        }
-        o[this.name].push(this.value || '');
-      } else {
-        o[this.name] = this.value || '';
-      }
-    });
-    return o;
 };
